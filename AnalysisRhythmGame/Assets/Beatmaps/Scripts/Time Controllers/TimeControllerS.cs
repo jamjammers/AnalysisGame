83070de7e997ccafb57eeb5bd7710aada03c4ee6
blhe <yummybreakfast46@gmail.com>
2025-11-30 22:45:35 -0600

    i give up,/ scorpio not working or mapped colorful not finished mapping but at least nadir works i think

---
+++ AnalysisRhythmGame/Assets/Beatmaps/Scripts/Time Controllers/TimeControllerS.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeControllerS : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+
+    public static float CTIME = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() { _audioSource.Play(); }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < NoteSpawnerS.notes.Count; i++) { 
+            if (NoteSpawnerS.prefabs[i] != null) {
+                NoteSpawnerS.prefabs[i].transform.position 
+                        = new Vector3(NoteSpawnerS.notes[i].prefab.transform.position.x, NoteSpawnerS.notes[i].prefab.transform.position.y, 40 * (NoteSpawnerS.notes[i].impactTime-CTIME)+NoteSpawnerS.notes[i].scale.z/2);
+            }
+        }
+        CTIME = _audioSource.time;
+    }
+}
