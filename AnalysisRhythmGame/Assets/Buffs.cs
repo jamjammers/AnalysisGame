34f4ba002c55ea5f61b566879289bca18c2fa2b2
James <jamesetam@gmail.com>
2025-12-08 18:08:33 -0600

    annotate

--- AnalysisRhythmGame/Assets/Buffs.cs
+++ AnalysisRhythmGame/Assets/Buffs.cs
@@ -11,29 +11,29 @@ public static class Buffs
             Coal: Idk i forgot what this debuff does
 
         Curse Tier: debuffs (with a slight counteracting buff)
-            cockroach and jam: 
-            mud pie: 
+            cockroach and jam: idk
+            mud pie: requires more precise timing + other things (think hard rock)
 
         Trash Tier: light vision debuff
-            fast food wrappers:
+            fast food wrappers: high contrast mode or sthn
 
         Uncommon Tier: drop rate buffs
             ketchup: more player exp (% boost)
             salt: more coin drops (% boost)
             soy sauce: more character exp (% boost)
             
-        Rare Tier: minor buffs (think pjsk)
-            burger:
-            chicken nuggets:
-            fries:
+        Rare Tier: minor (non-score related) buffs 
+            burger: starting hp ++
+            chicken nuggets: I'm rinning out of ideas
+            fries: out of ideas
             
-        Epic Tier: big buffs ?
-            boba:
-            ramen:
-            sushi:
+        Epic Tier: major buffs ?
+            boba: perfects give more score
+            ramen: slightly increases something
+            sushi: overall score boost
             
         rainbow tier: ultimate buffs
-            rainbow food:
+            rainbow food: does all of the drop rate, minor and major buffs (at half rate)
     */
     public static Dictionary<string, int> activeBuffs = new Dictionary<string, int>()
     {
