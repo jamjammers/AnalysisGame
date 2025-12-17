8fc42d29c560ef14eee6b6b24ec9ec47b8cc52dc
James <jamesetam@gmail.com>
2025-12-17 16:54:04 -0600

    revert example

--- AnalysisRhythmGame/Assets/Beatmaps/Scripts/Collision Handling/CollisionSingle.cs
+++ AnalysisRhythmGame/Assets/Beatmaps/Scripts/Collision Handling/CollisionSingle.cs
@@ -21,28 +21,28 @@ public class CollisionSingle : MonoBehaviour
         if (enter) {t2 += Time.deltaTime;}
         if (transform.position.z < speed/(-4) && !hit) {
             Destroy(gameObject);
-            // hitCategory.text = "Miss";
-            // ScoreController.BreakCombo();
+            hitCategory.text = "Miss";
+            ScoreController.BreakCombo();
         }
 
-        // if (hit && enter)
-        // {
-        //     float dif = Math.Abs(t2-t1);
-        //     if (dif < 0.05) { 
-        //         hitCategory.text = "Perfect"; 
-        //         ScoreController.ScorePerfect();
-        //     } else if (dif < .2) { 
-        //         hitCategory.text = "Great"; 
-        //         ScoreController.ScoreGreat();
-        //     } else { 
-        //         hitCategory.text = "Good";
-        //         ScoreController.ScoreGood();
-        //     }
+        if (hit && enter)
+        {
+            float dif = Math.Abs(t2-t1);
+            if (dif < 0.05) { 
+                hitCategory.text = "Perfect"; 
+                ScoreController.ScorePerfect();
+            } else if (dif < .2) { 
+                hitCategory.text = "Great"; 
+                ScoreController.ScoreGreat();
+            } else { 
+                hitCategory.text = "Good";
+                ScoreController.ScoreGood();
+            }
 
-        //     Destroy(gameObject);
-        // }
+            Destroy(gameObject);
+        }
     }
 
-    public void OnTriggerEnter(Collider other) { if (other.gameObject.tag == "Input") {enter = true;hitCategory.text = "Perfect"; ScoreController.ScorePerfect(); hitEffect.Play();} }
+    public void OnTriggerEnter(Collider other) { if (other.gameObject.tag == "Input") {enter = true;} }
     public void OnTriggerExit(Collider other) { if (other.gameObject.tag == "Input") {} }
 }
--- AnalysisRhythmGame/Assets/Beatmaps/Scripts/Collision Handling/CollisionSlider.cs
+++ AnalysisRhythmGame/Assets/Beatmaps/Scripts/Collision Handling/CollisionSlider.cs
@@ -20,37 +20,37 @@ public class CollisionSlider : MonoBehaviour
 
     public void Update()
     {
-        if (transform.position.z - transform.localScale.z/2 < speed/2 && Input.GetKeyDown(KeyMapping.keyMap[key])) t1 += Time.deltaTime;
+        if (transform.position.z - transform.localScale.z/2 < speed/2 && Input.GetKeyDown(KeyMapping.keyMap[key])) {t1 += Time.deltaTime; hit = true; }
         if (Input.GetKeyUp(KeyMapping.keyMap[key])) keyReleased = true;
         if (transform.position.z + transform.localScale.z/2 < speed/(-4)) {
             Destroy(gameObject);
-            // hitCategory.text = "Miss";
-            // ScoreController.BreakCombo();
+            hitCategory.text = "Miss";
+            ScoreController.BreakCombo();
         }
 
-        // if (exit && keyReleased)
-        // {
-        //     float keyHeldVsNoteLength = Math.Abs(transform.localScale.z/speed-t1);
-        //     Debug.Log("key entry tolerance: " + keyEntryTolerance + ", keyHeldVsNoteLength: " + keyHeldVsNoteLength);
-        //     if (keyEntryTolerance > 1.5) { 
-        //         hitCategory.text = "Miss"; 
-        //         ScoreController.BreakCombo();
-        //     } else if (keyEntryTolerance < 0.05 && keyHeldVsNoteLength < 0.15) { 
-        //         hitCategory.text = "Perfect"; 
-        //         ScoreController.ScorePerfect();
-        //     } else if (keyEntryTolerance < .3 && keyHeldVsNoteLength < .3) { 
-        //         hitCategory.text = "Great"; 
-        //         ScoreController.ScoreGreat();
-        //     } else { 
-        //         hitCategory.text = "Good";
-        //         ScoreController.ScoreGood();
-        //     }
+        if (exit && keyReleased)
+        {
+            float keyHeldVsNoteLength = Math.Abs(transform.localScale.z/speed-t1);
+            Debug.Log("key entry tolerance: " + keyEntryTolerance + ", keyHeldVsNoteLength: " + keyHeldVsNoteLength);
+            if (keyEntryTolerance > 1.5) { 
+                hitCategory.text = "Miss"; 
+                ScoreController.BreakCombo();
+            } else if (keyEntryTolerance < 0.05 && keyHeldVsNoteLength < 0.15) { 
+                hitCategory.text = "Perfect"; 
+                ScoreController.ScorePerfect();
+            } else if (keyEntryTolerance < .3 && keyHeldVsNoteLength < .3) { 
+                hitCategory.text = "Great"; 
+                ScoreController.ScoreGreat();
+            } else { 
+                hitCategory.text = "Good";
+                ScoreController.ScoreGood();
+            }
 
-        //     Destroy(gameObject);
-        // }
+            Destroy(gameObject);
+        }
     }
     public void OnTriggerStay(Collider other) { 
-        if (other.gameObject.tag == "Input") {
+        if (other.gameObject.tag == "Input" && hit) {
             // if (hit && !keyReleased) {
                 scoreTiming += Time.deltaTime;
                 if (scoreTiming > (60.0/(bpm*2)))
@@ -66,7 +66,6 @@ public class CollisionSlider : MonoBehaviour
     public void OnTriggerExit(Collider other) { if (other.gameObject.tag == "Input") exit = true;}
     public void OnTriggerEnter(Collider other)
     {
-        hitEffect.Play();
         keyEntryTolerance = Math.Abs((transform.position.z - transform.localScale.z/2)/speed-t1);
     }
 }
